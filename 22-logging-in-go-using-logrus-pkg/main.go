808a42176e45670cf227bdc6eafff62cbad5b082
Alex Pliutau <a.pliutau@gmail.com>
2021-04-08 13:21:30 +0200

    22

---
+++ 22-logging-in-go-using-logrus-pkg/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetFormatter(&log.JSONFormatter{})
+
+	logLevel, err := log.ParseLevel(os.Getenv("LOG_LEVEL"))
+	if err != nil {
+		logLevel = log.InfoLevel
+	}
+
+	log.SetLevel(logLevel)
+}
+
+func main() {
+	e := echo.New()
+	e.Use(loggingMiddleware)
+
+	e.GET("/isInt", func(c echo.Context) error {
+		a := c.QueryParam("a")
+
+		logCtx := log.WithField("a", a)
+		logCtx.Debug("parsing the string")
+
+		_, err := strconv.Atoi(a)
+		if err != nil {
+			logCtx.Debug("unable to parse a string")
+			return c.String(http.StatusBadRequest, "")
+		}
+		logCtx.Debug("string is parsed")
+
+		return c.String(http.StatusOK, "ok")
+	})
+
+	e.Start(":5050")
+}
+
+func loggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		start := time.Now()
+
+		res := next(c)
+
+		log.WithFields(log.Fields{
+			"method":     c.Request().Method,
+			"path":       c.Path(),
+			"status":     c.Response().Status,
+			"latency_ns": time.Since(start).Nanoseconds(),
+		}).Info("request details")
+
+		return res
+	}
+}
--- go.mod
+++ go.mod
@@ -9,10 +9,12 @@ require (
 	github.com/christianrondeau/go-wit v0.0.0-20170831224739-d540d3cc2c3d
 	github.com/cockroachdb/errors v1.8.3 // indirect
 	github.com/golang/protobuf v1.4.2
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/machinebox/sdk-go v0.3.1
 	github.com/nlopes/slack v0.6.0
 	github.com/pkg/errors v0.9.1
 	github.com/plutov/htgo-tts v0.0.0-20190202120930-874fa9dd16ff
+	github.com/sirupsen/logrus v1.8.1
 	github.com/tensorflow/tensorflow v2.1.0+incompatible
 	gocv.io/x/gocv v0.23.0
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974

fc0884b3111e251a3e4fcf319aebd6dc3f00aa4f
Alex Pliutau <a.pliutau@gmail.com>
2024-10-13 13:38:53 +0200

    28 final

--- 28-github-api/go.mod
+++ 28-github-api/go.mod
@@ -2,4 +2,9 @@ module github.com/plutov/packagemain/28-github-api
 
 go 1.23.2
 
-require golang.org/x/oauth2 v0.23.0
+require (
+	github.com/google/go-github/v65 v65.0.0
+	golang.org/x/oauth2 v0.23.0
+)
+
+require github.com/google/go-querystring v1.1.0 // indirect
--- 28-github-api/main.go
+++ 28-github-api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"time"
 
+	gogithub "github.com/google/go-github/v65/github"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 )
@@ -14,15 +17,48 @@ func main() {
 	mux.HandleFunc("/callback", callback)
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
-		log.Fatalf("unable to start server: %s", err.Error())
+		log.Fatalf("unable to start the server: %w", err)
 	}
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, getRedirectURL(), http.StatusTemporaryRedirect)
+}
+
+func callback(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+	// TODO: validate state
+	// state := r.URL.Query().Get("state")
+
+	conf := getOAuthConfig()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	token, err := conf.Exchange(ctx, code)
+	if err != nil {
+		log.Printf("unable to get token: %w", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	repos, err := getCurrentUserRepos(token.AccessToken)
+	if err != nil {
+		log.Printf("unable to get repos: %w", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	for _, r := range repos {
+		w.Write([]byte(r.GetFullName() + ", "))
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func getOAuthConfig() *oauth2.Config {
 	return &oauth2.Config{
-		// TODO: use env vars
-		ClientID:     "Ov23liNqJR9Ysv2Q967f",
-		ClientSecret: "da0cc01edf3043a3a11e806ff5dbfb9df80feb1a",
+		// TODO: use env vars instead
+		ClientID:     "Ov23li3gjjflBbmZ9pv5",
+		ClientSecret: "b9b47f092559a756154d50be302170aed0a6bcaa",
 		Scopes:       []string{"user:email", "repo:public_repo"},
 		Endpoint:     github.Endpoint,
 	}
@@ -33,10 +69,17 @@ func getRedirectURL() string {
 	return conf.AuthCodeURL("state")
 }
 
-func redirect(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, getRedirectURL(), http.StatusTemporaryRedirect)
-}
+func getCurrentUserRepos(accessToken string) ([]*gogithub.Repository, error) {
+	client := gogithub.NewClient(nil).WithAuthToken(accessToken)
 
-func callback(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ok"))
+	opt := &gogithub.RepositoryListByAuthenticatedUserOptions{
+		Affiliation: "owner",
+	}
+	opt.PerPage = 50
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	repos, _, err := client.Repositories.ListByAuthenticatedUser(ctx, opt)
+
+	return repos, err
 }

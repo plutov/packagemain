7e557287403009e17037a37245fcfd2204248e10
plutov <a.pliutau@gmail.com>
2024-11-24 20:00:14 +0100

    32 wip
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- 32-random-art-algorithm/main.go
+++ 32-random-art-algorithm/main.go
@@ -14,6 +14,8 @@ import (
 type Operation interface {
 	// a number of leaves which produce inputs for this operation
 	InputsCount() uint8
+	// set children of the operation
+	SetInputs([]Operation)
 	// always returns 3 elements for RGB
 	Eval(x float64, y float64) []float64
 }
@@ -23,10 +25,14 @@ type OpVarY struct{}
 type OpConstant struct {
 	constant float64
 }
+type OpColorMix struct {
+	inputs []Operation
+}
 
 func (o *OpVarX) InputsCount() uint8 {
 	return 0
 }
+func (o *OpVarX) SetInputs(inputs []Operation) {}
 func (o *OpVarX) Eval(x float64, y float64) []float64 {
 	return []float64{x, x, x}
 }
@@ -34,6 +40,7 @@ func (o *OpVarX) Eval(x float64, y float64) []float64 {
 func (o *OpVarY) InputsCount() uint8 {
 	return 0
 }
+func (o *OpVarY) SetInputs(inputs []Operation) {}
 func (o *OpVarY) Eval(x float64, y float64) []float64 {
 	return []float64{y, y, y}
 }
@@ -41,14 +48,33 @@ func (o *OpVarY) Eval(x float64, y float64) []float64 {
 func (o *OpConstant) InputsCount() uint8 {
 	return 0
 }
+func (o *OpConstant) SetInputs(inputs []Operation) {}
 func (o *OpConstant) Eval(x float64, y float64) []float64 {
 	return []float64{o.constant, o.constant, o.constant}
 }
 
+func (o *OpColorMix) InputsCount() uint8 {
+	return 3
+}
+func (o *OpColorMix) SetInputs(inputs []Operation) {
+	o.inputs = inputs
+}
+func (o *OpColorMix) Eval(x float64, y float64) []float64 {
+	r := o.inputs[0].Eval(x, y)[0]
+	g := o.inputs[1].Eval(x, y)[1]
+	b := o.inputs[2].Eval(x, y)[2]
+
+	return []float64{r, g, b}
+}
+
 func main() {
-	var phrase string
+	var (
+		phrase string
+		depth  int
+	)
 	// same phrase will always result in the same image
 	flag.StringVar(&phrase, "phrase", "", "phrase")
+	flag.IntVar(&depth, "depth", 3, "depth of graph")
 	flag.Parse()
 
 	h := md5.New()
@@ -58,17 +84,17 @@ func main() {
 	r := rand.New(rand.NewSource(int64(seed)))
 
 	// operations without inputs
-	var leaves = []Operation{&OpVarX{}, &OpVarY{}, &OpConstant{r.Float64()}}
-
+	opsNoLeaves := []Operation{&OpVarX{}, &OpVarY{}, &OpConstant{r.Float64()}, &OpColorMix{}}
 	// operations with inputs
-	// var ops = []Operation{}
+	opsWithLeaves := []Operation{&OpColorMix{}}
 
+	root := &OpColorMix{}
+	root.SetInputs(generateNodeChildren(root.InputsCount(), depth))
 	width := 100
 	height := 100
 
 	upLeft := image.Point{0, 0}
 	bottomRight := image.Point{width, height}
-
 	img := image.NewRGBA(image.Rectangle{upLeft, bottomRight})
 
 	for x := 0; x < width; x++ {
@@ -89,4 +115,9 @@ func main() {
 	// Encode as PNG.
 	f, _ := os.Create("image.png")
 	png.Encode(f, img)
+}
+
+// Given an expected children count and current depth generate the list of inputs
+func generateNodeChildren(count uint8, depth int) []Operation {
+
 }

b5ebff2f798a91754676750ef2e51e7007f41f67
plutov <a.pliutau@gmail.com>
2024-11-25 08:28:56 +0100

    32 wip
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- 32-random-art-algorithm/main.go
+++ 32-random-art-algorithm/main.go
@@ -7,6 +7,7 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math"
 	"math/rand"
 	"os"
 )
@@ -20,51 +21,101 @@ type Operation interface {
 	Eval(x float64, y float64) []float64
 }
 
+// OpVarX
 type OpVarX struct{}
-type OpVarY struct{}
-type OpConstant struct {
-	constant float64
-}
-type OpColorMix struct {
-	inputs []Operation
-}
 
 func (o *OpVarX) InputsCount() uint8 {
 	return 0
 }
+
 func (o *OpVarX) SetInputs(inputs []Operation) {}
+
 func (o *OpVarX) Eval(x float64, y float64) []float64 {
 	return []float64{x, x, x}
 }
 
+// OpVarY
+type OpVarY struct{}
+
 func (o *OpVarY) InputsCount() uint8 {
 	return 0
 }
+
 func (o *OpVarY) SetInputs(inputs []Operation) {}
+
 func (o *OpVarY) Eval(x float64, y float64) []float64 {
 	return []float64{y, y, y}
+}
+
+// OpConstant
+type OpConstant struct {
+	constant float64
 }
 
 func (o *OpConstant) InputsCount() uint8 {
 	return 0
 }
+
 func (o *OpConstant) SetInputs(inputs []Operation) {}
+
 func (o *OpConstant) Eval(x float64, y float64) []float64 {
 	return []float64{o.constant, o.constant, o.constant}
+}
+
+// OpColorMix
+type OpColorMix struct {
+	inputs []Operation
 }
 
 func (o *OpColorMix) InputsCount() uint8 {
 	return 3
 }
+
 func (o *OpColorMix) SetInputs(inputs []Operation) {
 	o.inputs = inputs
 }
+
 func (o *OpColorMix) Eval(x float64, y float64) []float64 {
 	r := o.inputs[0].Eval(x, y)[0]
 	g := o.inputs[1].Eval(x, y)[1]
 	b := o.inputs[2].Eval(x, y)[2]
 
 	return []float64{r, g, b}
+}
+
+// OpCircle
+type OpCircle struct {
+	centerX float64
+	centerY float64
+}
+
+func (o *OpCircle) InputsCount() uint8 {
+	return 0
+}
+
+func (o *OpCircle) SetInputs(inputs []Operation) {}
+
+func (o *OpCircle) Eval(x float64, y float64) []float64 {
+	h := math.Hypot(x-o.centerX, y-o.centerY)
+	return []float64{h, h, h}
+}
+
+// OpInverse
+type OpInverse struct {
+	inputs []Operation
+}
+
+func (o *OpInverse) InputsCount() uint8 {
+	return 1
+}
+
+func (o *OpInverse) SetInputs(inputs []Operation) {
+	o.inputs = inputs
+}
+
+func (o *OpInverse) Eval(x float64, y float64) []float64 {
+	v := o.inputs[0].Eval(x, y)
+	return []float64{1 - v[0], 1 - v[1], 1 - v[2]}
 }
 
 func main() {
@@ -83,13 +134,15 @@ func main() {
 	seed := binary.BigEndian.Uint64(h.Sum(nil))
 	r := rand.New(rand.NewSource(int64(seed)))
 
-	// operations without inputs
-	opsNoLeaves := []Operation{&OpVarX{}, &OpVarY{}, &OpConstant{r.Float64()}, &OpColorMix{}}
 	// operations with inputs
-	opsWithLeaves := []Operation{&OpColorMix{}}
+	opsWithLeaves := []Operation{&OpColorMix{}, &OpInverse{}}
+
+	// operations without inputs
+	opsNoLeaves := []Operation{&OpVarX{}, &OpVarY{}, &OpConstant{r.Float64()}, &OpCircle{r.Float64(), r.Float64()}}
 
 	root := &OpColorMix{}
-	root.SetInputs(generateNodeChildren(root.InputsCount(), depth))
+	root.SetInputs(generateNodeChildren(root.InputsCount(), depth, r, opsWithLeaves, opsNoLeaves))
+
 	width := 100
 	height := 100
 
@@ -100,11 +153,10 @@ func main() {
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
 			// Initial values from 0 to 1
-			fx := float64(x) / float64(width)
-			fy := float64(y) / float64(height)
+			xInit := float64(x) / float64(width)
+			yInit := float64(y) / float64(height)
 
-			i := r.Intn(len(leaves) - 1)
-			rgba := leaves[i].Eval(fx, fy)
+			rgba := root.Eval(xInit, yInit)
 			r := uint8(rgba[0] * 0xff)
 			g := uint8(rgba[1] * 0xff)
 			b := uint8(rgba[2] * 0xff)
@@ -118,6 +170,25 @@ func main() {
 }
 
 // Given an expected children count and current depth generate the list of inputs
-func generateNodeChildren(count uint8, depth int) []Operation {
+func generateNodeChildren(count uint8, depth int, r *rand.Rand, opsWithLeaves []Operation, opsNoLeaves []Operation) []Operation {
+	if count == 0 {
+		return []Operation{}
+	}
 
+	ops := []Operation{}
+	for i := 0; i < int(count); i++ {
+		var op Operation
+		if depth > 1 {
+			i := r.Intn(len(opsWithLeaves) - 1)
+			op = opsWithLeaves[i]
+		} else {
+			i := r.Intn(len(opsNoLeaves) - 1)
+			op = opsNoLeaves[i]
+		}
+
+		op.SetInputs(generateNodeChildren(op.InputsCount(), depth-1, r, opsWithLeaves, opsNoLeaves))
+		ops = append(ops, op)
+	}
+
+	return ops
 }

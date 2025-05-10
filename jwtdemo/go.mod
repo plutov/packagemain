c4788ef0aff1fc9513d1f4bda31308dd1f936d69
plutov <a.pliutau@gmail.com>
2025-05-10 23:04:40 +0200

    jot
    Signed-off-by: plutov <a.pliutau@gmail.com>

---
+++ jwtdemo/go.mod
@@ -0,0 +1,22 @@
+module jwtdemo
+
+go 1.24.2
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/labstack/echo-jwt/v4 v4.3.1
+	github.com/labstack/echo/v4 v4.13.3
+)
+
+require (
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
+)
---
+++ jwtdemo/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"crypto/rsa"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var (
+	publicKey  *rsa.PublicKey
+	privateKey *rsa.PrivateKey
+)
+
+func init() {
+	publicKeyData, err := os.ReadFile("./public_key.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	publicKey, err = jwt.ParseRSAPublicKeyFromPEM(publicKeyData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyData, err := os.ReadFile("./private_key.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	privateKey, err = jwt.ParseRSAPrivateKeyFromPEM(privateKeyData)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.POST("/login", login)
+
+	config := echojwt.Config{
+		NewClaimsFunc: func(c echo.Context) jwt.Claims {
+			return new(jwtCustomClaims)
+		},
+		SigningKey:    publicKey,
+		SigningMethod: jwt.SigningMethodRS256.Name,
+	}
+
+	g := e.Group("/api")
+	g.Use(echojwt.WithConfig(config))
+	g.GET("/greet", greet)
+
+	e.Start("127.0.0.1:4242")
+}
+
+type jwtCustomClaims struct {
+	jwt.RegisteredClaims
+}
+
+func login(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	if username != "package" || password != "main" {
+		return echo.ErrUnauthorized
+	}
+
+	claims := &jwtCustomClaims{
+		jwt.RegisteredClaims{
+			Subject:   username,
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+
+	t, err := token.SignedString(privateKey)
+	if err != nil {
+		return echo.ErrInternalServerError
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"token": t,
+	})
+}
+
+func greet(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*jwtCustomClaims)
+	sub := claims.Subject
+	return c.String(http.StatusOK, fmt.Sprintf("hi %s!", sub))
+}

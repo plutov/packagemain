465dbf238c3c76762ae4caabe593a6fb6b1ccf84
plutov <a.pliutau@gmail.com>
2026-03-09 15:26:25 +0100

    simplify

--- 2fa/mfaclient/totp.go
+++ 2fa/mfaclient/totp.go
@@ -4,6 +4,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/base32"
+	"encoding/binary"
 	"fmt"
 	"strings"
 	"time"
@@ -20,11 +21,9 @@ func generateHMAC(secret string, counter uint64) ([]byte, error) {
 		return nil, err
 	}
 
+	// The uint64 counter must be encoded as an 8-byte big-endian byte array
 	buf := make([]byte, 8)
-	for i := 7; i >= 0; i-- {
-		buf[i] = byte(counter & 0xff)
-		counter >>= 8
-	}
+	binary.BigEndian.PutUint64(buf, counter)
 
 	h := hmac.New(sha1.New, key)
 	h.Write(buf)
@@ -39,7 +38,6 @@ func truncate(hash []byte) int {
 		int(hash[offset+2])<<8 |
 		int(hash[offset+3])
 
-	// Return last 6 digits
 	return code % 1000000
 }
 
@@ -54,5 +52,5 @@ func GenerateTOTP(secret string) (string, int, error) {
 
 	timeRemaining := 30 - (int(time.Now().Unix()) % 30)
 
-	return fmt.Sprintf("%06d", code), timeRemaining, nil
+	return fmt.Sprintf("%d", code), timeRemaining, nil
 }

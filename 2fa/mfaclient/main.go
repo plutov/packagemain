c65cde4e08fd64c881fdf0964eb822fe60f1456a
plutov <a.pliutau@gmail.com>
2026-03-14 11:28:28 +0100

    remove accounts

--- 2fa/mfaclient/main.go
+++ 2fa/mfaclient/main.go
@@ -18,8 +18,8 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "mf2client",
-		Width:  1024,
-		Height: 768,
+		Width:  500,
+		Height: 700,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
